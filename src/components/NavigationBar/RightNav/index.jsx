873ed8ec8565f86d6ec890f9f9d610d2d5d6e805
Yuhyeon0516 <rladbgus0516@naver.com>
2023-06-28 21:50:21 +0900

    youtube logo asset 추가 및 NavigationBar 구조 작성

--- src/components/NavigationBar/RightNav/index.jsx
+++ src/components/NavigationBar/RightNav/index.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { MdApps, MdNotifications, MdVideoCall } from "react-icons/md";
+import ReactTooltip from "react-tooltip";
 
 const RightNav = () => {
-  return <div>RightNav</div>;
+  return (
+    <div className="buttons">
+      <button className="icon-container">
+        <MdVideoCall size={25} data-tip="Create" data-for="navbar" />
+      </button>
+      <button className="icon-container">
+        <MdApps size={25} data-tip="YouTube Apps" data-for="navbar" />
+      </button>
+      <button className="icon-container">
+        <MdNotifications size={25} data-tip="Notifications" data-for="navbar" />
+      </button>
+      <ReactTooltip id="navbar" backgroundColor="grey" effect="solid" delayHide={150} arrowColor="transparent" />
+    </div>
+  );
 };
 
 export default RightNav;
