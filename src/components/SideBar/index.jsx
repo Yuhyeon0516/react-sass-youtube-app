6873e7db3d87389a2ded2d38694e97ce750bc6b6
Yuhyeon0516 <rladbgus0516@naver.com>
2023-06-28 23:36:52 +0900

    SideBar Toggle 기능 추가

--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import useWindowSize from "../../helpers/useWindowSize";
 import SmallSideBar from "./SmallSideBar";
 import BigSideBar from "./BigSideBar";
+import { SidebarContext } from "../../context/SidebarContext";
 
 const SideBar = () => {
   const { width } = useWindowSize();
+  const { isToggled, setIsToggled } = useContext(SidebarContext);
 
-  return <>{width < 792 ? null : width < 1250 ? <SmallSideBar /> : <BigSideBar />}</>;
+  useEffect(() => {
+    width <= 1300 ? setIsToggled(false) : setIsToggled(true);
+  }, []);
+
+  return <>{width < 792 ? null : !isToggled ? <SmallSideBar /> : <BigSideBar />}</>;
 };
 
 export default SideBar;
