9550c83e605a74377e9f09355042ba820f5ac311
Yuhyeon0516 <rladbgus0516@naver.com>
2023-06-28 21:30:29 +0900

    pages내 .js를 .jsx로 전환 및 Component 구조 작성

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { Outlet, Route, Routes } from "react-router-dom";
+import MainPage from "./pages/MainPage";
+import SearchedVideosPage from "./pages/SearchedVideosPage";
+import VideoPage from "./pages/VideoPage";
+import NavigationBar from "./components/NavigationBar";
+import SideBar from "./components/SideBar";
+
+const Layout = () => {
+  return (
+    <>
+      <NavigationBar />
+      <SideBar />
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+};
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MainPage />} />
+          <Route path="/results/:inpu" element={<SearchedVideosPage />} />
+          <Route path="/video/:videoId" element={<VideoPage />} />
+        </Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
