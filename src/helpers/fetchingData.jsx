2c5505c4031a53074d9620baa456581017f01d0c
Yuhyeon0516 <rladbgus0516@naver.com>
2023-06-29 09:40:28 +0900

    YoutubeVideo 정보 가져오는 구조 작성

---
+++ src/helpers/fetchingData.jsx
@@ -0,0 +1,32 @@
+import axios from "../api/axios";
+
+export const getVideoInfo = async (videosArr) => {
+  try {
+    for (let video of videosArr) {
+      // Get Extra Video Info
+      const videoResponse = await axios.get(`/videos?part=snippet&part=contentDetails&part=player&part=statistics&id=${video.id.videoId}`);
+
+      Object.assign(video.snippet, { ...videoResponse.data.items[0].snippet });
+      video.extraInfo = Object.assign(
+        {},
+        videoResponse.data.items[0].tags,
+        videoResponse.data.items[0].contentDetails,
+        videoResponse.data.items[0].statistics,
+        videoResponse.data.items[0].player
+      );
+
+      // Get Channel Info
+      const channelReponse = await axios.get(`/channels?part=snippet&part=statistics&part=contentDetails&id=${video.snippet.channelId}`);
+
+      // Storing Fetched Data
+      const channelResultA = channelReponse.data.items[0].snippet;
+      const channelResultB = channelReponse.data.items[0].statistics;
+      const channelInfo = Object.assign({}, { ...channelResultA, ...channelResultB });
+      video.channelInfo = channelInfo;
+    }
+
+    return videosArr;
+  } catch (error) {
+    console.log(error);
+  }
+};
--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -1,6 +1,30 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import axios from "../../api/axios";
+import { getVideoInfo } from "../../helpers/fetchingData";
 
 const MainPage = () => {
+  const storedVideos = JSON.parse(localStorage.getItem("mainVideos"));
+  const [mainVideos, setMainVideos] = useState(storedVideos || []);
+
+  const getMainVideos = useCallback(async () => {
+    try {
+      if (!storedVideos) {
+        const response = await axios.get(`/search?part=snippet&maxResults=10&q=beautiful%20place`);
+        let videosArray = response.data.items;
+        videosArray = await getVideoInfo(videosArray);
+        setMainVideos(videosArray);
+
+        localStorage.setItem("mainVideos", JSON.stringify(videosArray));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  useEffect(() => {
+    getMainVideos();
+  }, [getMainVideos]);
+
   return <div>MainPage</div>;
 };
 
