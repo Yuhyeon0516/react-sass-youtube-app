f0c81d7bc25b7fb79df8f591abc052f4e01fb0e1
Yuhyeon0516 <rladbgus0516@naver.com>
2023-06-29 10:47:00 +0900

    VideoPage에 Youtube 영상 재생 기능 추가

--- src/pages/VideoPage/index.jsx
+++ src/pages/VideoPage/index.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { useLocation, useParams } from "react-router-dom";
+import Youtube from "react-youtube";
 
 const VideoPage = () => {
-  return <div>VideoPage</div>;
+  const { videoId } = useParams();
+  let location = useLocation();
+  const { state: currentVideo } = location;
+
+  const onPlayerReady = (e) => {
+    e.target.playVideo();
+  };
+
+  const opts = {
+    height: "390",
+    width: "640",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  return (
+    <div>
+      <Youtube className="youtube-plater" videoId={videoId} onPlay={onPlayerReady} opts={opts} autoPlay />
+    </div>
+  );
 };
 
 export default VideoPage;
