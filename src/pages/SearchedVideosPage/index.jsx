260744127866ff2c44bcf7c18a8ec6f96f1a1ce9
Yuhyeon0516 <rladbgus0516@naver.com>
2023-06-29 13:10:30 +0900

    SearchedVideosPage 구성(Styling X)

--- src/pages/SearchedVideosPage/index.jsx
+++ src/pages/SearchedVideosPage/index.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { SearchContext } from "../../context/SearchContext";
+import { getVideoInfo } from "../../helpers/fetchingData";
+import VideoCard from "../../components/VideoCard";
+import { SidebarContext } from "../../context/SidebarContext";
 
 const SearchedVideosPage = () => {
-  return <div>SearchedVideosPage</div>;
+  const [searchedVideos, setSearchedVideos] = useState([]);
+  const { searchQuery } = useContext(SearchContext);
+  const { setIsToggled } = useContext(SidebarContext);
+
+  const loadVideoInfo = async () => {
+    setIsToggled(true);
+    const videos = await getVideoInfo(searchQuery.videos);
+    setSearchedVideos(videos);
+  };
+
+  useEffect(() => {
+    loadVideoInfo();
+  }, [loadVideoInfo]);
+
+  const searchedVideosMarkUp = searchedVideos?.map((video) => (
+    <VideoCard
+      key={video.id.videoId}
+      id={video.id.videoId}
+      video={video}
+      img={video.snippet.thumbnails.medium.url}
+      info={video.snippet}
+      eInfo={video.extraInfo}
+      channelInfo={video.channelInfo}
+    />
+  ));
+
+  return <section>{searchedVideosMarkUp}</section>;
 };
 
 export default SearchedVideosPage;
